efee5fe3b0bbbf6364df7cf0966abd30dfcb5c93
EC2 Default User <ec2-user@ip-172-31-44-147.us-east-2.compute.internal>
2025-09-29 18:17:31 +0000

    python code

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,14 @@
-print ("helloworld")
+import tkinter as tk
+from tkinter import messagebox
+
+def say_hello():
+    messagebox.showinfo("Greeting", "Hello from GUI!")
+
+root = tk.Tk()
+root.title("Sample GUI")
+
+button = tk.Button(root, text="Click Me", command=say_hello)
+button.pack(pady=20)
+
+root.mainloop()
+
